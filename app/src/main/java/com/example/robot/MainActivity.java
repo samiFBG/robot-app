9d0a2f4752edccc7addce894a4d41ea5ff2d891e
samiFBG <62943665+samiFBG@users.noreply.github.com>
2020-12-27 17:32:11 +0100

    bleuttoth

--- app/src/main/java/com/example/robot/MainActivity.java
+++ app/src/main/java/com/example/robot/MainActivity.java
@@ -1,53 +1,119 @@
 package com.example.robot;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.robot.bluetooth.DeviceListActivity;
+import com.example.robot.bluetooth.CommunicationsTask;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
-    int countrevgauche = 0;
-    int countrevdroit=0;
-    String reversegauche ="";
-    String reversedroit ="";
-
-
+    int countrevGauche = 0;
+    int countrevDroit=0;
+    String reverseGauche ="";
+    String reverseDroit ="";
+    TextView pmGauche ;
+    TextView pmDroit ;
+    int progresseDroit = 0;
+    int progresseGauche = 0 ;
+    TextView btetat ;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent myIntent = getIntent(); // gets the previously created intent
+        String firstKeyName = myIntent.getStringExtra("Connect"); // will return "FirstKeyValue"
         setContentView(R.layout.activity_app);
+       pmgauche();
+       pmdroit();
+       if (firstKeyName == "true"){
+           btetat = findViewById(R.id.btetat);
+           btetat.setText("connecter");
+       }else{
+           btetat = findViewById(R.id.btetat);
+           btetat.setText("deconnecter");
+       }
     }
+    public void pmdroit(){
+        pmDroit = findViewById(R.id.pmdroit);
+        SeekBar mdroit = findViewById(R.id.mdroit);
+        mdroit.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progresseDroit = progress;
+                pmDroit.setText(reverseDroit + progresseDroit);
+            }
 
-    public  void revgauche(View v){
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
-        if (countrevgauche == 0){
-            countrevgauche = 1;
-            reversegauche ="rev:";
-        }else{
-            countrevgauche = 0;
-            reversegauche ="";
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+public void pmgauche(){
+    pmGauche = findViewById(R.id.pmgauche);
+    SeekBar mdgauche = findViewById(R.id.mgauche) ;
+    mdgauche.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            progresseGauche = progress;
+            pmGauche.setText( reverseGauche + progresseGauche);
         }
 
-        TextView textView = findViewById(R.id.pgauche);
-        textView.setText(reversegauche);
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+
+    });
+}
+    public  void revgauche(View v){
+        if (countrevGauche == 0){
+            countrevGauche = 1;
+            reverseGauche="rev :";
+        }else{
+            countrevGauche = 0;
+            reverseGauche="";
+        }
+        pmGauche.setText( reverseGauche + progresseGauche);
     }
 
     public  void revdroit(View v){
 
-        if (countrevdroit == 0){
-            countrevdroit = 1;
-            reversedroit ="rev";
+        if (countrevDroit== 0){
+            countrevDroit = 1;
+            reverseDroit="rev :";
         }else{
-            countrevdroit = 0;
-            reversedroit ="";
+            countrevDroit = 0;
+            reverseDroit ="";
         }
-        TextView textView = findViewById(R.id.pdroit);
-        textView.setText(reversedroit);
-    }
+        pmDroit.setText(reverseDroit + progresseDroit);
 
+    }
+    public void connect(View v){
+        Intent intent = new Intent(this, DeviceListActivity.class);
+        startActivity(intent);
+    }
 
 }
